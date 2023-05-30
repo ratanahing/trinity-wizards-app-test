57055bcd7f161a8122e8df9276ac7a6cafb421e8
Ratana <ratanahing@gmail.com>
2023-05-30 10:43:13 +0800

    UI for screen 1 and 2.

--- app/src/main/java/com/trinitywizards/apptest/screen/Screen1Activity.kt
+++ app/src/main/java/com/trinitywizards/apptest/screen/Screen1Activity.kt
@@ -1,12 +1,17 @@
 package com.trinitywizards.apptest.screen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.trinitywizards.apptest.R
 
 class Screen1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen1)
+
     }
+
+
 }
